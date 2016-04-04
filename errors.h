ba25262776ee05bd6af1e913fb89bc6670c90173
fastfieros <fastfieros@gmail.com>
2016-04-04 20:33:00 +0000

    Added arg checking

---
+++ errors.h
@@ -0,0 +1,4 @@
+
+#define ERR_OK                    (0)
+#define ERR_INVALID_ARGUMENTS     (10)
+#define ERR_UNKNOWN               (20)
--- main.c
+++ main.c
@@ -1,6 +1,44 @@
+/* Includes local to this project */
+#include "errors.h"
+
+/* System Includes */
 #include <stdio.h>
+
+/* function prototypes */
+int checkArguments(int argc, char **argv);
+void usage(char *argv0);
 
 int main(int argc, char **argv)
 {
-    return 0;
+    int err;
+    
+    err = checkArguments(argc, argv);
+    
+    if (ERR_OK == err)
+    {
+        //todo Parse Header here
+    }
+    else
+    {
+        usage(argv[0]); 
+    }
+    
+    return -err;
+}
+
+int checkArguments(int argc, char **argv)
+{
+    /* for this program there are NO acceptable arguments */
+    if (argc == 1)
+    {
+        return ERR_OK;
+    }
+    
+    return ERR_INVALID_ARGUMENTS;
+}
+
+void usage(char *argv0)
+{
+    fprintf(stderr, "usage: %s\n", argv0);
+    fprintf(stderr, "Must run without any arguments\n");
 }
